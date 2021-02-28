b27f50b00456397418910ff40647d44782e179e5
lukas <lukas@neurotech.lt>
2021-02-28 23:15:31 +0200

    ammend

--- py_model/gameBoard.py
+++ py_model/gameBoard.py
@@ -9,6 +9,34 @@ Created on Wed Feb 24 17:22:20 2021
 import numpy as np
 
 
+def tic_tac_win_check(board):
+    """
+    Check if board has a winner.
+    Return 0 - incomplete, 1 - has a winner, 2 - full board, but a tie.
+    """
+    rowsum = np.abs(np.sum(board, axis=0))
+    # style comment: refactoring would take longer...
+    if np.any(rowsum == 3):
+        return 1
+    
+    colsum = np.abs(np.sum(board, axis=1))
+    if np.any(colsum == 3):
+        return 1
+    
+    # diagonal check
+    if np.abs(np.trace(board)) == 3:
+        return 1
+
+    # anti-diagonal
+    if np.abs(np.sum(np.flipud(board).diagonal())) == 3:
+        return 1
+
+    if np.any(board==0):
+        return 2
+    
+    return 0
+    
+
 class Board:
     def __init__(self, **kwargs):
         self.size = int(kwargs.get('size', 3))
@@ -33,5 +61,23 @@ class Board:
             raise Exception("Invalid move. Tried to move to: {}".format(move))
         
         self.state[move] = self.current_player
+        self.last_move = move
+        
+        self._local_win_check()
+                
+        
         self.current_player *= -1
         self.last_move = move
+        self.availables = self.update_availables(self.last_move)
+    
+    def update_availables(self):
+        pass
+    
+        
+    def _local_win_check(self):
+        # get position!
+        cb_x = self.last_move[0]//self.size
+        cb_y = self.last_move[1]//self.size
+        current_board = self.state[cb_x:cb_x+self.size,
+                              cb_y:cb_y+self.size]
+        has_winner = tic_tac_win_check(current_board)
